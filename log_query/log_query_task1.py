ac505034c39e8bb2a18a42ac1286f848ea92e95c
Vincenzo <92443685+vincenzojrs@users.noreply.github.com>
2022-10-20 12:15:10 +0200

    Update log_query_task1.py
    dropped difference, and created the final_csv

--- log_query/log_query_task1.py
+++ log_query/log_query_task1.py
@@ -1,37 +1,23 @@
-# !!!!!!TO BE DONE!!!!!!; DELETE THIS COMMENT WHEN DONE AND UPDATE THE QUERY BELOW
-# FILTERING FOR OBSERVATION IN 2021...AND DO WE NEED SOME OTHER FILTERS? CAN'T REMEMBER
-# PLEASE ADD ALSO A .TO_CSV TO OUTPUT THE FINAL CSV AND UPLOAD TO "data" FOLDER "final_csv_task1.csv"
-
-##########################################################################################
-
 # Run first the code to create the database, available in data/database_creation.py. Use that file, which is the most updated
 
 query = pd.read_sql("""
 WITH d AS (
   SELECT comp_date, product_id, MIN(price) AS min, MAX(price) AS max
   FROM prices
-   GROUP BY comp_date, product_id
-), 
-c AS (
-  SELECT DATE(date) as date, type, position, seller, product_id
-  FROM concatenato
+  GROUP BY comp_date, product_id
 )
 
-SELECT p.comp_date, p.product_id, p.seller_id, d.min, p.price, d.max, c.type, c.position
+SELECT p.comp_date, p.product_id, p.seller_id, d.min, p.price, d.max
 FROM prices as p
 LEFT JOIN d ON p.comp_date = d.comp_date AND p.product_id = d.product_id
-LEFT JOIN c ON p.comp_date = c.date AND p.seller_id = c.seller AND p.product_id = c.product_id
 """,
-                    conn)
-print(query)
+                         conn)
 
 # Ragioni per non tenere colonne concatenato
-
 # SELECT * FROM CONCAT WHERE DATE(date) = '2021-07-07' AND product_id = '128776' AND seller = 23
 # >>> Vuoto
-
-print(query.type.value_counts())
-print(query.position.value_counts())
+# print(query.type.value_counts())
+# print(query.position.value_counts())
 
 # Verifica consistency prezzi
 
@@ -40,3 +26,9 @@ print(query['difference'].value_counts())
 
 query['difference'] = np.where((query['price'] <= query['max']) & (query['price'] >= query['min']), 1, 0)
 print(query['difference'].value_counts())
+
+query.drop(columns=['difference'], inplace = True)
+
+query.head()
+
+query.to_csv('final_csv_task1.csv')
